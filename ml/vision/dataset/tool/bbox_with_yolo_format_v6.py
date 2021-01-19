f3d3604aa1f41ade2c07f9e06d46bb1b3714f39b
aiwizard <munhwan.park@gmail.com>
2021-01-19 20:08:09 +0900

    supported another image format

--- ml/vision/dataset/tool/bbox_with_yolo_format_v6.py
+++ ml/vision/dataset/tool/bbox_with_yolo_format_v6.py
@@ -5,6 +5,10 @@ import time
 import matplotlib.pyplot as plt
 
 
+splash_image = "m5.jpg"
+#ext_str = ""
+
+
 def get_name():
     f = open("food100.names", 'r')
     names = f.readlines()
@@ -25,9 +29,9 @@ def draw_bbox(filepath, idx):
 
     img = cv2.imread(filepath, cv2.IMREAD_COLOR)
     dh, dw, _ = img.shape
-    print("{}, {}".format(dw, dh))
+    print("\t{}, {}".format(dw, dh))
 
-    labelfile = filepath.replace(".JPG", ".txt")
+    labelfile = filepath.replace(ext_str, ".txt")
     fl = open(labelfile, 'r')
     labels = fl.readlines()
     fl.close()
@@ -36,7 +40,7 @@ def draw_bbox(filepath, idx):
         # Split string to float
         #_, x, y, w, h = map(float, label.split(' '))
         food_class, x, y, w, h = map(float, label.split(' '))
-        print("{}, {}, {}, {}".format(x,y,w,h))
+        print("\t{}, {}, {}, {}".format(x,y,w,h))
 
         # Taken from https://github.com/pjreddie/darknet/blob/810d7f797bdb2f021dbe65d2524c2ff6b8ab5c8b/src/image.c#L283-L291
         # via https://stackoverflow.com/questions/44544471/how-to-get-the-coordinates-of-the-bounding-box-in-yolo-object-detection#comment102178409_44592380
@@ -54,7 +58,7 @@ def draw_bbox(filepath, idx):
         if b > dh - 1:
             b = dh - 1
 
-        print("{}, {}, {}, {}".format(l,t,r,b))
+        print("\t{}, {}, {}, {}".format(l,t,r,b))
         #cv2.rectangle(img, (l, t), (r, b), (255, 0, 255), 2)
         cv2.rectangle(img, (l, t, r-l, b-t), (255, 0, 255), 1)
         #cv2.putText(img, food[food_class], (l, t-8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, 1);
@@ -65,7 +69,7 @@ def draw_bbox(filepath, idx):
     #plt.imshow(img)
     #plt.show()
 
-    img = cv2.resize(img, (800,600))
+    #img = cv2.resize(img, (800,600))
     caption = "bbox on image - {}, {}".format(filepath, idx+1)
     cv2.imshow(caption, img)
     cv2.moveWindow(caption, 100,50)
@@ -81,7 +85,7 @@ def draw_bbox(filepath, idx):
 
 
 def draw_bbox_file(filepath):
-    img = cv2.imread('m5.jpg', cv2.IMREAD_COLOR)
+    img = cv2.imread(splash_image, cv2.IMREAD_COLOR)
     cv2.imshow('image', img)
 
     while True:
@@ -98,20 +102,26 @@ def draw_bbox_file(filepath):
     cv2.destroyAllWindows()
 
 
+
 def draw_bbox_dir(dirpath):
     filelist = []
-
+    #print("ext_str: ", ext_str)
     for f in os.listdir(dirpath):
         full_filename = os.path.join(dirpath, f)
         ext = os.path.splitext(full_filename)[-1]
-        if ext == '.JPG':
+        if ext == ext_str:
             filelist.append(full_filename)
 
-    img = cv2.imread('m5.jpg', cv2.IMREAD_COLOR)
+    if len(filelist) == 0:
+        print("There is no {} file".format(ext_str))
+        return
+
+    img = cv2.imread(splash_image, cv2.IMREAD_COLOR)
     cv2.imshow('image', img)
 
-    i=0
+    i=-1 #처음 'd' 누르면, 무조건 next 이므로
     size = len(filelist)
+    print(filelist, size, i)
     while True:
         key = cv2.waitKey(0)
     
@@ -119,17 +129,15 @@ def draw_bbox_dir(dirpath):
             print("bye")
             break
         elif key == ord('a'):
-            if( i-1 > 0 ):
-                i -= 1
-            else:
-                print("first file")
+            i -= 1
         elif key == ord('d'):
-            if( i+1 < size ):
-                i += 1
-            else:
-                print("last file")
+            i += 1
         else:
-            print("skip")
+            print("You can press (a | d)")
+
+
+        if i < 0: i=0
+        if i > size-1: i=size-1
 
         print(filelist[i])
         cv2.destroyAllWindows()
@@ -139,45 +147,18 @@ def draw_bbox_dir(dirpath):
     cv2.destroyAllWindows()
 
 
-############################################
-'''
-def show_img(is_next):
-    if is_next == 1:
-        print("-->")
-        # 다음 이미지
-    elif is_next == 2:
-        print("<--")
-        # 이전 이미지
-
 
 def main():
-    img = cv2.imread('m5.jpg', cv2.IMREAD_COLOR)
-    cv2.imshow('image', img)
+    if(len(sys.argv) != 3):
+        print("usage: {} <file | dir> JPG|PNG|...".format(sys.argv[0]))
+        return
 
-
-    while True:
-        key = cv2.waitKey(0)
-    
-        if key == ord('q'):
-            print("bye")
-            break
-        elif key == ord('a'):
-            show_img(1)
-        elif key == ord('d'):
-            show_img(2)
-        else:
-            print("skip")
-
-    cv2.destroyAllWindows()
-'''
-############################################
-
-
-def main():
     filepath = sys.argv[1]
-    if( filepath.find(".JPG") == -1):
-        if( filepath.find(".png") == -1):
-            draw_bbox_dir(filepath)    # dir path
+    global ext_str
+    ext_str = ".{}".format(sys.argv[2])
+
+    if( filepath.find(ext_str) == -1):
+        draw_bbox_dir(filepath)    # dir path
     else:
         draw_bbox_file(filepath)    # file path
 
